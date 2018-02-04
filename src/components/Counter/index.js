9437241f54194267e6378388274de9263097dd92
Santiago Estrago <santiago.estrago@moove-it.com>
2018-02-04 01:28:24 -0300

    Style changes

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,11 +1,19 @@
 import React, { PureComponent } from 'react';
 import './Counter.css';
 
+const MAX = 10;
+
 class Counter extends PureComponent {
   render() {
     const { count } = this.props;
+    const isMoreThanMax = count > MAX;
     return (
-      <div className='Counter'>
+      <div className='Counter' style={{ color: isMoreThanMax ? 'red' : 'white' }}>
+        { isMoreThanMax &&
+          <span className="Counter-warning" role="img" aria-label="warning">
+            ⚠️
+          </span>
+        }
         <h1>{ count }</h1>
       </div>
     );
